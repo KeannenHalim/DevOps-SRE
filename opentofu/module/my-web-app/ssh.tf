24ec6cc20e64e6f29a8e9655d14c922231ee0825
KeannenHalim <keannenr@gmail.com>
2025-09-28 15:18:53 +0700

    change local file to local sensitive file

--- opentofu/module/my-web-app/ssh.tf
+++ opentofu/module/my-web-app/ssh.tf
@@ -3,7 +3,7 @@ resource "tls_private_key" "rsa_key"{
     rsa_bits = 4096
 }
 
-resource "local_file" "private_key" {
+resource "local_sensitive_file" "private_key" {
   filename = pathexpand("~/.ssh/${var.env}-${var.region}-web-key.pem")
   content  = tls_private_key.rsa_key.private_key_pem
   file_permission = "0600"
